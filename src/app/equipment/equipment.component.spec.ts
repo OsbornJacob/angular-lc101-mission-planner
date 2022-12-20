0889b63247872bde4517837066e5083a10928e9b
Jake <ozborn97@gmail.com>
2022-12-20 16:19:38 -0500

    Studio-3-Almost-Done

--- src/app/equipment/equipment.component.spec.ts
+++ src/app/equipment/equipment.component.spec.ts
@@ -1,3 +1,4 @@
+import { analyzeAndValidateNgModules } from '@angular/compiler';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EquipmentComponent } from './equipment.component';
